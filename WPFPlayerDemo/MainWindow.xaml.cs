e90026c60bd41934bce206fe00c9561b7052c0b3
nandehutuzn <120902259@qq.com>
2016-09-22 21:05:40 +0800

    主界面完成

--- WPFPlayerDemo/MainWindow.xaml.cs
+++ WPFPlayerDemo/MainWindow.xaml.cs
@@ -24,11 +24,11 @@ namespace WPFPlayerDemo
         {
             InitializeComponent();
 
-            if (!Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_CPSPEAKERS, IntPtr.Zero))
-                MessageBox.Show("Bass 初始化失败 " + Bass.BASS_ErrorGetCode().ToString());
-            string file = @"E:\KwDownload\song\胡夏-那些年.mp3";
-            int stream = Bass.BASS_StreamCreateFile(file, 0L, 0L, BASSFlag.BASS_SAMPLE_FLOAT);
-            Bass.BASS_ChannelPlay(stream, true);
+            //if (!Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_CPSPEAKERS, IntPtr.Zero))
+            //    MessageBox.Show("Bass 初始化失败 " + Bass.BASS_ErrorGetCode().ToString());
+            //string file = @"E:\KuGou\Christina Aguilera - We Remain.mp3";
+            //int stream = Bass.BASS_StreamCreateFile(file, 0L, 0L, BASSFlag.BASS_SAMPLE_FLOAT);
+            //Bass.BASS_ChannelPlay(stream, true);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -42,6 +42,76 @@ namespace WPFPlayerDemo
         }
 
         private void drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LastButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayListButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MuteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VolumeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VolumeBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Model_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuPlay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuDesktopLyric_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void about(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
         {
 
         }
